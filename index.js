3d86f58d96302aac648ae72246ec71e48652fe11
William Warner Burke <williamburke121@gmail.com>
2022-02-08 12:18:05 -0500

    commit tree d756271f415347b6589d9870e532f20960dd0c03

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then((json) => renderBooks(json)); 
   
+  
+
 }
 
 function renderBooks(books) {
